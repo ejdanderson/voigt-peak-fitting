17f927365f357f81c9b7e7e35ffce694c341176a
Evan Anderson <ejdanderson@gmail.com>
2017-11-13 14:56:00 -0700

    Minor cleanup

--- fitvoigt.m
+++ fitvoigt.m
@@ -26,7 +26,7 @@ function [f,G,fit,out] = fitvoigt(p,y,x,sigma),
   
     % insist a1, a2, a3, tau1l, tau2l > 0
     % @TODO is this really needed? Should be for all amps/taus
-	if (amp(1) < 0) | (amp(2) < 0) | (amp(3) < 0), 
+    if (amp(1) < 0) | (amp(2) < 0) | (amp(3) < 0), 
         f=1e18;
         fit=zeros(size(y));
         G=[];
@@ -34,7 +34,7 @@ function [f,G,fit,out] = fitvoigt(p,y,x,sigma),
         return;
     end 
   
-	if (tau_l(1) < 0) | (tau_l(2) < 0), 
+    if (tau_l(1) < 0) | (tau_l(2) < 0), 
         f=1e25;
         fit=zeros(size(y));
         G=[];
@@ -48,18 +48,10 @@ function [f,G,fit,out] = fitvoigt(p,y,x,sigma),
         voigts(:,i) = amp(i) * fvoigt(x - pos(i), [tau_l(i), tau_g(i)]);
     end
     
-  %{
-  voight1 = a1*voight(x-pos1,[tau1l,tau1g]);
-  voight2 = a2*voight(x-pos2,[tau2l,tau2g]);
-  voight3 = a3*voight(x-pos3,[tau3l,tau3g]);
-  voight4 = a4*voight(x-pos4,[tau4l,tau4g]);
-  voight5 = a5*voight(x-pos5,[tau5l,tau5g]);
-  %}
-   
 	baseline= offset + curve * (x - most).^2;
     
     fit = zeros(length(x),1);
-	for i=1:length(amp)
+    for i=1:length(amp)
         fit = fit + voigts(:, i);
     end
     
